beecde2a7788d879930ac8d54358558c1a42a734
Ifrah Pervaiz <ifrahsyed529@gmail.com>
2024-11-21 11:28:17 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+age=eval(input("Enter your Age:"))
+if age<2:
+    print("You are a Baby")
+elif age>=2 and age<4:
+    print("You are a Toddler")
+elif age>=13 and age<20:
+    print("You are a Teenager")
+elif age>=20 and age<65:
+    print("You are an Adult")
+elif age>=65:
+    print("You are a Elder")
